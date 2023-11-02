f0b0cb854fc1aa20e9cf985c15cc02da36635e1e
Kajal-Pandhare <kajal.pandhare14@gmail.com>
2023-11-02 18:31:45 +0530

    FirstApp

---
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.app2;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
